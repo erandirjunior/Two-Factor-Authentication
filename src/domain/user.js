c70fc9b593e01d02b41072591d09fd4210d976d1
Erandir <antonio.soares@multilaser.com.br>
2022-10-05 14:16:08 -0300

    Private attributes

--- src/domain/user.js
+++ src/domain/user.js
@@ -1,6 +1,18 @@
 module.exports = class User {
+    #email;
+    #password;
+
     constructor({email, password}) {
-        this.email = email;
-        this.password = password;
+        this.#email = email;
+        this.#password = password;
+    }
+
+
+    get email() {
+        return this.#email;
+    }
+
+    get password() {
+        return this.#password;
     }
 }
