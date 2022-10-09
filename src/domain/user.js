3f0a86c73a86cf364ccd4b360f3048cec7c85fc9
Erandir <antonio.soares@multilaser.com.br>
2022-10-09 14:38:58 -0300

    Added validation in fields id, email and  password

--- src/domain/user.js
+++ src/domain/user.js
@@ -1,10 +1,35 @@
 module.exports = class User {
+    token;
+    emailToken;
+
+    #id;
     #email;
     #password;
+    #expireDate;
 
-    constructor({email, password}) {
+    constructor({id, email, password, token, emailToken, expireDate}) {
+        if (!email || !password) {
+            throw Error('Fields email end password must be filled!');
+        }
+
+        this.#id = id;
         this.#email = email;
         this.#password = password;
+        this.token = token;
+        this.emailToken = emailToken;
+        this.#expireDate = expireDate;
+    }
+
+    get id() {
+        if (!this.#id) {
+            throw Error('Field id must be filled!');
+        }
+
+        return parseInt(this.#id);
+    }
+
+    get expireDate() {
+        return this.#expireDate;
     }
 
     get email() {
