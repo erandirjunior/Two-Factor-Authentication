3b79b288b4598f72becd7456b134f05008934d31
Erandir <antonio.soares@multilaser.com.br>
2022-10-19 20:28:19 -0300

    Load necessary services

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const { loadEnv } = require('./src/infra/env/load-env');
 const { init } = require('./src/infra/http/hapi/server');
-loadEnv();
-init();
+const { Repository } = require("./src/infra/persistence/repository");
+const { loadModel } = require("./src/infra/persistence/model");
+// const Database = require('./src/infra/persistence/database');
+
+async function run() {
+    await loadEnv();
+    init();
+}
+
+run();
