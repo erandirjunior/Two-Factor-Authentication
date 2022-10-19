853f75a7999f58ac2f92709ba84011cf2b6a6676
Erandir <antonio.soares@multilaser.com.br>
2022-10-19 20:29:16 -0300

    Removed unnecessary imports

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const { loadEnv } = require('./src/infra/env/load-env');
 const { init } = require('./src/infra/http/hapi/server');
-const { Repository } = require("./src/infra/persistence/repository");
-const { loadModel } = require("./src/infra/persistence/model");
-// const Database = require('./src/infra/persistence/database');
 
 async function run() {
     await loadEnv();
-    init();
+    await init();
 }
 
 run();
