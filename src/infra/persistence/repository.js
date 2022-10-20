73f79e760ea049413580b5d4494240e92160f1d8
Erandir <antonio.soares@multilaser.com.br>
2022-10-20 10:39:22 -0300

    Implemented token repository

--- src/infra/persistence/repository.js
+++ src/infra/persistence/repository.js
@@ -17,7 +17,7 @@ class Repository extends IRepository {
         });
 
         if (!result) {
-            throw Error('Error find user');
+            throw Error('User not found!');
         }
 
         return new User({
@@ -44,6 +44,53 @@ class Repository extends IRepository {
     }
 }
 
+class TokenRepository extends ITokenRepository {
+    #model;
+
+    constructor(model) {
+        super();
+        this.#model = model
+    }
+
+    updateExpiredFieldToTrue(id) {
+        throw Error('Must be implemented');
+    }
+
+    async findByToken(token) {
+        const result = await this.#model.findOne({ where: {
+                token: token.token,
+                email_token: token.emailToken,
+            }
+        });
+
+        if (!result) {
+            throw Error('User not found!');
+        }
+
+        return new User({
+            id: result.dataValues.id,
+            token: result.dataValues.token,
+            emailToken: result.dataValues.email_token,
+            email: result.dataValues.email,
+            password: result.dataValues.password,
+            expired: result.dataValues.expired
+        })
+    }
+
+    async updateExpiredFieldToTrue(id) {
+        const data = {
+            expired: true
+        }
+
+        await this.#model.update(data, {
+            where: {
+                id
+            }
+        });
+    }
+}
+
 module.exports = {
-    Repository
+    Repository,
+    TokenRepository
 }
